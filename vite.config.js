c334e7f408ddb533de0dce249e1353147c2f278c
John Dews-Flick <jddfski@gmail.com>
2023-03-18 22:59:15 -0700

    final?

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,4 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
@@ -13,6 +12,9 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  build: {
+    target: 'esnext'
   },
   assetsInclude: ["**/*.bin"]
 })
